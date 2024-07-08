1a546f7aba1a72d62f23c8a2282c2c00ff5f377a
TEJASWI SINGH <tejaswisingh14@gmail.com>
2024-07-07 22:30:15 -0400

    Initial Change

---
+++ helper_functions.py
@@ -0,0 +1,12 @@
+import datetime
+
+# In this example, the strftime() function is called on the now datetime object with the format string "%d-%m-%Y %I:%M:%S %p". The %d, %m, and %Y are directives representing day, month, and year respectively. The %I directive represents the hour in 12-hour format, %M represents minutes, %S represents seconds, and %p represents the AM/PM indicator.
+# When you run this code, it will display the current date and time in the specified format, including the AM/PM indicator.
+# Here's an example output: "Formatted Date Time is:  07-07-2024 10:26:22 PM"
+def formatted_datetime_to_am_pm():
+    datetime_now = datetime.datetime.now()
+    return datetime_now.strftime("%d-%m-%Y %I:%M:%S %p")
+     
+if __name__ == "__main__":
+    formatted_datetime = formatted_datetime_to_am_pm()
+    print(f'Formatted Date Time is:  {formatted_datetime}')
